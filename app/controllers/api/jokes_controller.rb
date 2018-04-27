30a23cbbd6ad2269a432b40db4dc840ed8bea2ec
Rachel <rlmandelbaum@gmail.com>
2018-04-26 21:34:01 -0400

    updates to api

--- app/controllers/api/jokes_controller.rb
+++ app/controllers/api/jokes_controller.rb
@@ -2,7 +2,7 @@ class Api::JokesController < ApplicationController
   before_action :set_joke, only: [:show, :update, :destroy]
 
   def index
-   render json: Joke.order('id DEC')
+   render json: Joke.order('id ASC')
   end
 
   def create
